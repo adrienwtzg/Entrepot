4636008884ff7cd0c7a6e85c28b24f1f07e3ffd1
adrienwtzg <adrien.wtzg@eduge.ch>
2016-11-02 09:37:05 +0100

    Revert "ajout 3 boutons au Hangar"
    This reverts commit cb31629673f1778bcb08983cbd52cee6bc0e466f.

--- farmVilleV2/farmVilleV2/frmHangar.Designer.cs
+++ farmVilleV2/farmVilleV2/frmHangar.Designer.cs
@@ -28,37 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(87, 235);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(184, 137);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Acheter 100 plants";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(495, 235);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(184, 137);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Acheter 100 plants";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(268, 180);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(231, 192);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Acheter 1000 plants";
-            this.button3.UseVisualStyleBackColor = true;
             // 
             // frmHangar
             // 
@@ -67,9 +37,6 @@
             this.BackgroundImage = global::farmVilleV2.Properties.Resources.ImageHangar;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(765, 532);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
             this.Name = "frmHangar";
             this.Text = "Hangar";
             this.ResumeLayout(false);
@@ -77,10 +44,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
 
     }
 }
