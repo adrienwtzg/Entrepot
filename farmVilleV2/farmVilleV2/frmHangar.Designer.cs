a0e5180e1477baf391a4aea46ef24c423136c3aa
adrienwtzg <adrien.wtzg@eduge.ch>
2016-11-02 14:53:54 +0100

    Amélioration Hangar

--- farmVilleV2/farmVilleV2/frmHangar.Designer.cs
+++ farmVilleV2/farmVilleV2/frmHangar.Designer.cs
@@ -28,38 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.btnAcheter50Plants = new System.Windows.Forms.Button();
+            this.btnAcheter100Plants = new System.Windows.Forms.Button();
+            this.btnAcheter1000Plants = new System.Windows.Forms.Button();
             this.btnMarchandArme = new System.Windows.Forms.Button();
+            this.tbxAffichagePlants = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbxAffichageArgent = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // button1
+            // btnAcheter50Plants
             // 
-            this.button1.Location = new System.Drawing.Point(87, 235);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(184, 137);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Acheter 100 plants";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnAcheter50Plants.Location = new System.Drawing.Point(87, 235);
+            this.btnAcheter50Plants.Name = "btnAcheter50Plants";
+            this.btnAcheter50Plants.Size = new System.Drawing.Size(184, 137);
+            this.btnAcheter50Plants.TabIndex = 0;
+            this.btnAcheter50Plants.Text = "Acheter 50 plants\r\n(250 $)\r\n";
+            this.btnAcheter50Plants.UseVisualStyleBackColor = true;
+            this.btnAcheter50Plants.Click += new System.EventHandler(this.btnAcheter50Plants_Click);
             // 
-            // button2
+            // btnAcheter100Plants
             // 
-            this.button2.Location = new System.Drawing.Point(495, 235);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(184, 137);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Acheter 100 plants";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnAcheter100Plants.Location = new System.Drawing.Point(495, 235);
+            this.btnAcheter100Plants.Name = "btnAcheter100Plants";
+            this.btnAcheter100Plants.Size = new System.Drawing.Size(184, 137);
+            this.btnAcheter100Plants.TabIndex = 1;
+            this.btnAcheter100Plants.Text = "Acheter 100 plants \r\n(500 $)";
+            this.btnAcheter100Plants.UseVisualStyleBackColor = true;
+            this.btnAcheter100Plants.Click += new System.EventHandler(this.btnAcheter100Plants_Click);
             // 
-            // button3
+            // btnAcheter1000Plants
             // 
-            this.button3.Location = new System.Drawing.Point(268, 180);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(231, 192);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Acheter 1000 plants";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnAcheter1000Plants.Location = new System.Drawing.Point(268, 180);
+            this.btnAcheter1000Plants.Name = "btnAcheter1000Plants";
+            this.btnAcheter1000Plants.Size = new System.Drawing.Size(231, 192);
+            this.btnAcheter1000Plants.TabIndex = 2;
+            this.btnAcheter1000Plants.Text = "Acheter 1000 plants\r\n(5000 $)";
+            this.btnAcheter1000Plants.UseVisualStyleBackColor = true;
+            this.btnAcheter1000Plants.Click += new System.EventHandler(this.btnAcheter1000Plants_Click);
             // 
             // btnMarchandArme
             // 
@@ -70,6 +77,44 @@
             this.btnMarchandArme.Text = "Marchand Armes";
             this.btnMarchandArme.UseVisualStyleBackColor = true;
             // 
+            // tbxAffichagePlants
+            // 
+            this.tbxAffichagePlants.Enabled = false;
+            this.tbxAffichagePlants.Location = new System.Drawing.Point(653, 11);
+            this.tbxAffichagePlants.Name = "tbxAffichagePlants";
+            this.tbxAffichagePlants.ReadOnly = true;
+            this.tbxAffichagePlants.Size = new System.Drawing.Size(100, 20);
+            this.tbxAffichagePlants.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(580, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 18);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Plants :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(580, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 18);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Argent :";
+            // 
+            // tbxAffichageArgent
+            // 
+            this.tbxAffichageArgent.Enabled = false;
+            this.tbxAffichageArgent.Location = new System.Drawing.Point(653, 44);
+            this.tbxAffichageArgent.Name = "tbxAffichageArgent";
+            this.tbxAffichageArgent.ReadOnly = true;
+            this.tbxAffichageArgent.Size = new System.Drawing.Size(100, 20);
+            this.tbxAffichageArgent.TabIndex = 7;
+            // 
             // frmHangar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -77,22 +122,32 @@
             this.BackgroundImage = global::farmVilleV2.Properties.Resources.ImageHangar;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(765, 532);
+            this.Controls.Add(this.tbxAffichageArgent);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbxAffichagePlants);
             this.Controls.Add(this.btnMarchandArme);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnAcheter1000Plants);
+            this.Controls.Add(this.btnAcheter100Plants);
+            this.Controls.Add(this.btnAcheter50Plants);
             this.Name = "frmHangar";
             this.Text = "Hangar";
+            this.Load += new System.EventHandler(this.frmHangar_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnAcheter50Plants;
+        private System.Windows.Forms.Button btnAcheter100Plants;
+        private System.Windows.Forms.Button btnAcheter1000Plants;
         private System.Windows.Forms.Button btnMarchandArme;
+        private System.Windows.Forms.TextBox tbxAffichagePlants;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbxAffichageArgent;
 
     }
 }
--- farmVilleV2/farmVilleV2/frmHangar.cs
+++ farmVilleV2/farmVilleV2/frmHangar.cs
@@ -12,9 +12,65 @@ namespace farmVilleV2
 {
     public partial class frmHangar : Form
     {
+        int Plants = 0;
+        int Argent = frmFarmVille.Argent;
         public frmHangar()
         {
             InitializeComponent();
+        }
+
+        private void frmHangar_Load(object sender, EventArgs e)
+        {
+            ActualisationNbPlants();
+            ActualisationArgent();
+        }
+        private void ActualisationNbPlants()
+        {
+            tbxAffichagePlants.Text = Plants.ToString();
+        }
+        private void ActualisationArgent()
+        {
+            tbxAffichageArgent.Text = frmFarmVille.Argent.ToString();
+            Argent = frmFarmVille.Argent;
+        }
+
+        private void btnAcheter50Plants_Click(object sender, EventArgs e)
+        {
+            ActualisationArgent();
+            if (Argent >= 250)
+            {
+                Argent -= 250;
+                frmFarmVille.Argent -= 250;
+                ActualisationArgent();
+                Plants += 50;
+                ActualisationNbPlants();
+            }
+        }
+
+        private void btnAcheter100Plants_Click(object sender, EventArgs e)
+        {
+            ActualisationArgent();
+            if (Argent >= 500)
+            {
+                Argent -= 500;
+                frmFarmVille.Argent -= 500;
+                ActualisationArgent();
+                Plants += 100;
+                ActualisationNbPlants();
+            }
+        }
+
+        private void btnAcheter1000Plants_Click(object sender, EventArgs e)
+        {
+            ActualisationArgent();
+            if (Argent >= 5000)
+            {
+                Argent -= 5000;
+                frmFarmVille.Argent -= 5000;
+                ActualisationArgent();
+                Plants += 1000;
+                ActualisationNbPlants();
+            }
         }
     }
 }
